43e71d0a03113a24e4431797aac5f6915284a613
your name <your email>
2020-02-24 17:16:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var img_squ_1 = document.getElementById("img_squ_1");
+var cover_1 = document.getElementById("cover_1");
+var img_squ_2 = document.getElementById("img_squ_2");
+var cover_2 = document.getElementById("cover_2");
+var img_squ_3 = document.getElementById("img_squ_3");
+var cover_3 = document.getElementById("cover_3");
+var img_squ_4 = document.getElementById("img_squ_4");
+var cover_4 = document.getElementById("cover_4");
+
+img_squ_1.onmouseover = function(){
+    cover_1.style.display = "block";
+};
+img_squ_1.onmouseout = function(){
+    cover_1.style.display = "none";
+};
+
+
+
+
